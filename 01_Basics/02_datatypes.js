d22df9844b6c3133f631d828742225d4bc09f1bc
Patel Kaushal Narayanbhai <patelkaushal2807@gmail.com>
2024-01-12 10:31:09 +0530

    Datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(5 + 5) we are node js not browser
+console.log( 3 + 5)
+
+console.log("Kaushal")
+// ECMA is official website for javascript and MDN is referable website
+
+let name = "Kaushal"
+let age = 20
+let isLoggedIn = false
+let state;
+
+/*
+Data types:-
+
+   1.number ==> 2 to power 53
+   2.bigint
+   3.string ==> ' ' or " "
+   4.boolean ==> true / false
+   5.null ==> standalone value  it is represented empty value,null can be assign also
+   6.undefined ==> when the value is not defined
+   7.symbol ==> unique
+*/
+
+// object
+
+console.log(typeof"Hitesh");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
