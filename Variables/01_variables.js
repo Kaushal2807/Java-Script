b1151c2778bd0572dfbba79da1640cdfd43cf461
Patel Kaushal Narayanbhai <patelkaushal2807@gmail.com>
2024-01-12 10:04:42 +0530

    Variables

---
+++ Variables/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "abc@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+let accountState;
+
+// const cannot change
+// accountId = 2 not allowed
+// Prefer of issue in block scope and functional scope 
+
+accountEmail = "def@gmail.com"
+accountPassword = "565656"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
