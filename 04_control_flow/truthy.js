06e07e0f0792255ed17b97100e280d9e50649d46
Patel Kaushal Narayanbhai <patelkaushal2807@gmail.com>
2024-01-16 17:31:17 +0530

    Truthy

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -27,3 +27,21 @@ const emptyObj = {}
 if (Object.keys(emptyObj).length === 0){
     console.log("Object is empty");
 }
+
+
+// Nullish Coalescing Operator(??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+console.log(val1);
+
+// Terniary Operator 
+//  condtion ? true : false
+
+const iceTeaprice = 100 
+iceTeaprice <= 80 ? console.log("Less than 80"):console.log("More than 80");
